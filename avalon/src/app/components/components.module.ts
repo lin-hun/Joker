2571d0fa5fcf77d4b30c13443d04205a10a614fd
yan.zhuang <>
2019-09-04 11:51:19 +0800

    feat:remove unused asset

--- avalon/src/app/components/components.module.ts
+++ avalon/src/app/components/components.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core'
 import Menu from './menu/menu'
 import {MatMenuModule} from '@angular/material/menu'
 import {MatToolbarModule} from '@angular/material'
+import {MatIconModule} from '@angular/material/icon'
 import {BrowserModule} from '@angular/platform-browser'
 
 @NgModule({
@@ -11,6 +12,7 @@ import {BrowserModule} from '@angular/platform-browser'
   imports: [
     MatMenuModule,
     MatToolbarModule,
+    MatIconModule,
     BrowserModule
   ],
   exports: [
--- avalon/src/app/components/menu/menu.ts
+++ avalon/src/app/components/menu/menu.ts
@@ -1,4 +1,6 @@
 import { Component,OnInit } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MatIconRegistry} from '@angular/material/icon';
 @Component({
   selector: 'Menu',
   templateUrl: './menu.html',
@@ -10,4 +12,9 @@ export default class Menu{
     {name:'Layers'},
     {name:'Components'}
   ]
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'thumbs-up',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/examples/thumbup-icon.svg'));
+  }
 }
