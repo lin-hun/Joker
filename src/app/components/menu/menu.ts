5ff2d1228521456ff06432f6ed40dab8484c4403
yan.zhuang <>
2019-09-06 14:27:07 +0800

    feat:reformat menu

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,7 +1,7 @@
-import { Component,OnInit } from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {DomSanitizer} from '@angular/platform-browser'
 import {MatIconRegistry} from '@angular/material/icon'
-import {Router} from '@angular/router'
+import {Router,NavigationEnd} from '@angular/router'
 import {menu} from "../../config/menu";
 
 @Component({
@@ -9,31 +9,42 @@ import {menu} from "../../config/menu";
   templateUrl: './menu.html',
   styleUrls: ['./menu.scss']
 })
-export default class Menu implements OnInit{
+export default class Menu implements OnInit {
   data = {
-    menus :menu.map(v=>{
+    menus: menu.map(v => {
       return {
         ...v,
-        nav:`/${v.path}`,
+        nav: `/${v.path}`,
       }
     })
   }
   methods = {
-    nav:(menu)=>{
+    nav: (menu) => {
       this.router.navigate([menu.nav])
     }
   }
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer,private router:Router) {
+  routerSubscribe(){
     let me = this
-    me.data.menus.forEach((v)=>{
-      iconRegistry.addSvgIcon(
-        v.icon,
-        sanitizer.bypassSecurityTrustResourceUrl(`assets/${v.icon}.svg`))
+    me.router.events.subscribe(e=>{
+      if(e instanceof NavigationEnd){
+        console.log(me.router.url)
+      }
     })
+  }
+  registerSvg(){
+    let me = this
+    me.data.menus.forEach((v) => {
+      me.iconRegistry.addSvgIcon(
+        v.icon,
+        me.sanitizer.bypassSecurityTrustResourceUrl(`assets/${v.icon}.svg`))
+    })
+  }
+  constructor(private router: Router,private iconRegistry: MatIconRegistry,private sanitizer: DomSanitizer,) {
+    let me = this
+    me.routerSubscribe()
+    me.registerSvg()
   }
   ngOnInit() {
     let me = this
-    console.log(me.router)
-    console.log(me.router.url,'router')
   }
 }
